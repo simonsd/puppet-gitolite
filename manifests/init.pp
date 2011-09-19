ef09088b5f7aaeda045b28a9258b3dc5e7da6a74
Dave Simons <dave@inuits.be>
2011-09-19 23:10:23 +0200

    add in sshkey param and use it for gl-setup sshkey
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,8 @@ import 'classes/*'
 class gitolite (
 	$root = '/var/lib/gitolite/',
 	$user = 'gitolite',
-	$group = 'gitolite'
+	$group = 'gitolite',
+	$sshkey = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAqG2peGlJK7ca53uOQ8GDW4TPlsS1H13rdGU6Paz675QSLrZMfUSb1G3BQ8fn0kQ3kYaKiYZ9H7Kb+UUuep6dRB1LnaknNCQ6bVm0YcPbhY1g95L7WjMxBNWGNDPKhyt0ArWXasHQOHmDZQIyPUWJpRMw9r9Ovq5zBho71ESW2UeER5dKSozdlDIRF1KpmgQwniPRev584p3HhRrJfq/s41aYVo+TqtGHVMsPt5R47E67QgWGC5sZmTRUFDDLALxDXRC+OmyWN4ipsy56VEX5zcmCkBWqhonsuqbm9LBKXUGNhiwVL54HlypOh8enASsY6v/8FG+hKCX5Vpty+DvFQw== gitolite@test'
 ) {
 	class {
 		'gitolite::packages':
