868a9acf3739f8c30e8f715c0867ff6b86e608fa
Dave Simons <dave@inuits.be>
2011-09-19 23:10:23 +0200

    add in sshkey param and use it for gl-setup sshkey
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/core.pp
+++ manifests/classes/core.pp
@@ -1,7 +1,14 @@
 class gitolite::core {
+	file {
+		'gl-sshkey':
+			path => "$gitolite::root/.ssh/id_rsa.pub",
+			owner => "$gitolite::user",
+			group => "$gitolite::user",
+	}
+
 	exec {
 		'gl-setup':
-			command => "gl-setup $sshkey",
+			command => "$gitolite::sshkey > gl-setup",
 			user => "$gitolite::user",
 			cwd => "$gitolite::root";
 	}
