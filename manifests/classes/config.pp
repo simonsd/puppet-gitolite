4b21245848177580f24eb06676510f72a248394c
Dave Simons <dave@inuits.be>
2011-10-26 00:14:47 +0200

    add confdir resource and param
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/config.pp
+++ manifests/classes/config.pp
@@ -1,19 +1,27 @@
 class gitolite::config {
 	file {
+		"$gitolite::confdir":
+			ensure => directory,
+			owner => "$gitolite::user",
+			group => "$gitolite::group",
+			mode => "0750";
+
 		'gitolite.rc':
 			ensure => present,
-			path => "$gitolite::root/.gitolite.rc",
+			path => "$gitolite::confdir/gitolite.rc",
 			owner => "$gitolite::user",
 			group => "$gitolite::group",
 			mode => '0644',
-			content => template('gitolite/gitolite.rc.erb');
+			content => template('gitolite/gitolite.rc.erb'),
+			require => File["$gitolite::confdir"];
 
 		'gitolite.conf':
 			ensure => present,
-			path => "$gitolite::root/.gitolite/conf/gitolite.conf",
+			path => "$gitolite::confdir/gitolite.conf",
 			owner => "$gitolite::user",
 			group => "$gitolite::group",
 			mode => '0600',
-			content => template('gitolite/gitolite.conf.erb');
+			content => template('gitolite/gitolite.conf.erb'),
+			require => File["$gitolite::confdir"];
 	}
 }
