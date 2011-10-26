6c0607c4d642f80326b9f0c5dfd3fd13a83300e0
Dave Simons <dave@inuits.be>
2011-10-26 02:12:31 +0200

    use relative path for gl-setup ssh key
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/setup.pp
+++ manifests/classes/setup.pp
@@ -9,7 +9,7 @@ class gitolite::setup {
 
 	exec {
 		'gl-setup':
-			command => "gl-setup -q $gitolite::root/.ssh/admin.pub",
+			command => "gl-setup -q .ssh/admin.pub",
 			user => "$gitolite::user",
 			cwd => "$gitolite::root",
 			require => File['admin_sshkey'];
