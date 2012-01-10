a9ea4032409308595dd6fb374507e1a3159f29a9
Dave Simons <dave@inuits.eu>
2012-01-10 16:57:23 +0100

    rewrite pkg installation section to accomodate systems without the yaourt:pkg resource
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -1,21 +1,17 @@
 class gitolite::packages {
-	@package {
-		'gitolite':
-			ensure => present,
-			require => $::operatingsystem ? {
-				default => undef,
-				centos => Yumrepo['epel'],
-			},
-	}
-
-	@yaourt::pkg {
-		'gitolite':
-			name => 'gitolite-git';
-	}
-
-	if $::operatingsystem == 'archlinux' {
-		realize(Yaourt::Pkg['gitolite'])
-	} else {
-		realize(Package['gitolite'])
-	}
+  if $::operatingsystem == 'archlinux' {
+    yaourt::pkg {
+      'gitolite':
+        name => 'gitolite-git';
+    }
+  } else {
+    package {
+      'gitolite':
+        ensure => present,
+        require => $::operatingsystem ? {
+          default => undef,
+          centos => Yumrepo['epel'],
+        },
+    }
+  }
 }
