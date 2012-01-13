921898a99146b1338bc9c845222a1d609df19a31
Dave Simons <dave@inuits.eu>
2012-01-13 10:48:03 +0100

    switch user in command itself, rather than with user param, updated comment to explain workaround
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/classes/setup.pp
+++ manifests/classes/setup.pp
@@ -1,12 +1,12 @@
 class gitolite::setup {
-#	for some wacky reason this doesn't work when running with puppet,
-#	even though it does manually. running as root solves it.
+# the su ${gitolite::user} stuff is dirty, but it works.
+# don't try to replace it with user => "${gitolite::user}"; because that breaks.
+# for some reason i think it's not properly su-ing to the gitolite user.
 
 	exec {
 		'gl-setup':
-			command => "gl-setup -q ${gitolite::admin_pubkey}",
+			command => "su ${gitolite::user} -c \"gl-setup -q ${gitolite::admin_pubkey}\"",
 			onlyif => "test -f ${gitolite::admin_pubkey}",
 			unless => "test -f ${gitolite::home}/.gitolite/conf/gitolite.conf";
-#			user => "$gitolite::user";
 	}
 }
